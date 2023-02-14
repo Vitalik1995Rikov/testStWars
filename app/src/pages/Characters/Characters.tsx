26b4fc242dccc760b5a41b10cea5cde3f695bc04
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2023-02-14 11:34:07 +0300

    feat: Added tailwind [task-2]

---
+++ app/src/pages/Characters/Characters.tsx
@@ -0,0 +1,5 @@
+function Characters() {
+    return <div className="text-6xl">Characters</div>
+}
+
+export default Characters;
---
+++ app/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/pages/**/*.{html,js,ts,tsx}',
+  './components/**/*.{html,js,ts, tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
